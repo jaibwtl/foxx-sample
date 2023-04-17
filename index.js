3908344083a901a4fbda57d8fef3d8590fa33b9f
jaibwtl <128355134+jaibwtl@users.noreply.github.com>
2023-04-17 12:12:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ router.get('/hello-world', function (req, res) {
 .response(['text/plain'], 'A generic greeting.')
 .summary('Generic greeting')
 .description('Prints a generic greeting.');
+
+
+// continued
+const joi = require('joi');
+
+router.get('/hello/:name', function (req, res) {
+  res.send(`Hello ${req.pathParams.name}`);
+})
+.pathParam('name', joi.string().required(), 'Name to greet.')
+.response(['text/plain'], 'A personalized greeting.')
+.summary('Personalized greeting')
+.description('Prints a personalized greeting.');
